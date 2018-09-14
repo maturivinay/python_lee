d95390564f59072390820126c1c435b2c86bc5ad
maturivinay <31376403+maturivinay@users.noreply.github.com>
2018-09-14 17:52:49 -0500

    commit
    commit

--- ICP_4/Most_repeated_value.py
+++ ICP_4/Most_repeated_value.py
@@ -16,3 +16,11 @@ print(randnums)
 # Printing out the entire list
 print("Most repeated value is:",statistics.mode(randnums))
 # Printing out mode of given data-set
+import numpy as np
+from collections import Counter
+from statistics import mode
+
+array = np.random.randint(0,21,15)
+print(array)
+counts = np.bincount(array)
+print (np.argmax(counts))
